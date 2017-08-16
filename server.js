fb9ad66d546f08f2257a3d10e16d30ef92a33e2d
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-08-16 19:10:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articlesTree= {
-    articleOne: {
+    'article-one': {
         title:"The first article",
         articlenum:"Hello World this is my first article",
         date:"16 Aug, 2017",
@@ -18,7 +18,7 @@ var articlesTree= {
             This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.</p>
             `
                  },
-    articleTwo: {
+    'article-Two': {
         title:"The Second article",
         articlenum:"Hello World this is my first article",
         date:"16 Aug, 2017",
@@ -30,7 +30,7 @@ var articlesTree= {
             This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.This is the content of the first article.</p>
             `
                  },
-    articleThree:{
+   'article-Three':{
         title:"The Third article",
         articlenum:"Hello World this is my first article",
         date:"16 Aug, 2017",
