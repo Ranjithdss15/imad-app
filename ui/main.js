10d38840d5d0189d398dde1979b9b945c77490e2
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-08-19 13:46:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ imageele.onclick = function () {
     
 };*/
 
-var b1element=document.getElementById('b1');
+var b1element= document.getElementById("b1");
 b1element.onclick = function () {
       alert('Started');
 };
