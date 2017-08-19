b87bd628a8e5261f64287642c3b819a706a0152e
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-08-19 15:53:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,8 +20,10 @@ function function1() {
     
       alert('Started');
       
-      var b1color=document.getElementsByClassName('color');
-  b1color.style.bgcolor= '#f90053';
+      var b1color=document.getElementById('color');
+      var color1;
+  //color1.style.bgcolor= '#f90053';
+   //var c2=document.getElementsByClassName("body");
 
 }
 
