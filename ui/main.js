b40f056bdbaf7b6b1d9e7d24b2dad5fa1d4522ef
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-08-19 11:10:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 console.log('Loaded!');
 alert('Hi,this is main.js');
-var element = document.getElementById('main-text')
+var element = document.getElementById('main-text');
 
-element.innerText('Hi this is the editted html')
+
 
 element.innerText = `
 first line
 second line
 <br>
-third line`
+third line`;
