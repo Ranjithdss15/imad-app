a06d3f5fd28612178835eab2634891c07fddd97c
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-08-19 12:43:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,15 +6,19 @@ function moveRight() {
     imageele.style.marginLeft = marginLeft+'px' ;
     if(marginLeft== '300px')
     'break';
-    
-}
+    }
 imageele.onclick = function () {
     var interval =setInterval(moveRight,50);
     
-   
 };
+var b1element=document.getElementById(b1);
 
-
+b1element.onClick = function () {
+    alert('Started');
+   /*var b1color=document.getElementsByClassName('body');
+  b1color.style.bgcolor= '#f90053';
+    */
+};
 //alert('Hi,this is main.js');
 //var element = document.getElementById('main-text');
 //element.innerText = `
