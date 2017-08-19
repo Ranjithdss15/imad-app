386d3548304cd3fe819730d4af9ee84e81060cfd
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-08-19 16:33:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,6 @@
 console.log('Loaded!');
 
 //window.onload = function(){
-
 var imageele = document.getElementById('image');
 marginLeft=0;
 function moveRight() {
@@ -12,10 +11,12 @@ function moveRight() {
     }
 imageele.onclick = function () {
     var interval =setInterval(moveRight,50);
-    
-};
+    };
 var b1element= document.getElementById("b1");
-b1element.onclick = function1;
+b1element.onclick = function2()
+{
+    var interval1 =setInterval(function1,5);
+}
 function function1() {
       alert('Started');
        var interval =setInterval(b1color,50);
