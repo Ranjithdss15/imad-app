2f9a64f161c10a79d312ae74d8993da4dae723f0
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-08-19 11:11:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,5 @@
 console.log('Loaded!');
 alert('Hi,this is main.js');
 var element = document.getElementById('main-text');
-
-
-
 element.innerText = `
-first line
-second line
-<br>
-third line`;
+The main.js file is executed now`;
