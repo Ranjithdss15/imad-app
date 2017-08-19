6f3fec895e5b3db0b1a5462959819626afe4ffa3
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-08-19 16:36:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,10 +14,11 @@ imageele.onclick = function () {
     };
 var b1element= document.getElementById("b1");
 b1element.onclick = function() {
-    var interval1 =setInterval(function1,5);
+     alert('Started');
+    var interval1 =setInterval(function1,50);
 };
 function function1() {
-      alert('Started');
+     
        var interval =setInterval(b1color,50);
         var b1color=document.querySelector("body");
         b1color.style.backgroundColor= '#66ff33';
