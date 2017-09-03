98fca0093fbac5b51dc1ff617e55b80537f304a5
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-09-03 17:22:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,8 +53,9 @@ submitclick.onclick = function() {
 var comments1 = [ 'name1', 'name2','name3','name4'];
 for(temp=0;temp<=comments1.length;temp++) {
     list = list + comments[temp] + '';
+    listcomments.innerHTML = list;
 }
-listcomments.innerHTML = list;
+
 };
 
 
