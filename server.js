c83a19b54e1b0ca97199cd5ffded52a3b06fd5fe
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-09-09 04:37:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/submit-comment/:', function (req, res) {
+app.get('/submit-comment/:comment', function (req, res) {
     var comments=[];
     var comment= req.params.comment;
     comments.push(comment);
