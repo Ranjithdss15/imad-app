e9bbbc7641d767950509f2138942d725ff576372
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-09-09 04:13:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,6 @@ submitclick.onclick = function() {
     var listcomments = document.getElementById('commentsoutput');
     var comments1 = [ 'name1', 'name2','name3','name4'];
     for(var temp=0;temp<comments1.length;++temp) {
-     //  listcomments.innerHTML = ( list += comments1[temp] );
          listcomments.innerHTML = ( list += ('<li>' + comments1[temp]));
         }
     };
