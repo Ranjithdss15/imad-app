afa46839a6d6f6229b7e15fa6c9be3860ddbe3c8
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-09-09 13:18:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool =reqeust('pg').Pool;
+//var Pool =reqeust('pg').Pool;
 
 var config = {
  user: 'ranjithdss15',
