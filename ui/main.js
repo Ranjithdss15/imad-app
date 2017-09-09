bf59433004139888978fcd89c5b76f38594a800f
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-09-09 06:04:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,30 +12,32 @@ imageele.onclick = function () {
     var interval =setInterval(moveRight,50);
     };
     
-   var commentsip = document.getElementById('commentsinput');
-    commentsipvalue=commentsip.value;
+
 var commentsop = document.getElementById('commentsoutput');
 var submitclick = document.getElementById('commentsubmit');
    submitclick.onclick = function() {
 
- 
-    var request =new XMLHttpRequest();
-    request.onreadystatechange =function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            if(request.status ===200){
-                var list = ' ';
-            var comments =  request.responseText;
-            comments=JSON.parse(comments);
-      for(var temp=0;temp<comments.length;++temp) {
-         list += ('<li>' + comments[temp]);
-          commentsop.innerHTML = list.toString();
-        }
-        } 
-        }
-    };
-        request.open('GET','http://ranjithdss15.imad.hasura-app.io/submit-comment?comment='+commentsipvalue,true);
-        request.send(null);
-
+    var commentsip = document.getElementById('commentsinput');
+    commentsipvalue=commentsip.value;
+    if(commentsipvalue!=='')
+    {
+        var request =new XMLHttpRequest();
+        request.onreadystatechange =function() {
+            if(request.readyState === XMLHttpRequest.DONE) {
+                if(request.status ===200){
+                    var list = ' ';
+                var comments =  request.responseText;
+                comments=JSON.parse(comments);
+          for(var temp=0;temp<comments.length;++temp) {
+             list += ('<li>' + comments[temp]);
+              commentsop.innerHTML = list.toString();
+            }
+            } 
+            }
+        };
+            request.open('GET','http://ranjithdss15.imad.hasura-app.io/submit-comment?comment='+commentsipvalue,true);
+            request.send(null);
+    }
 };
 var b1element= document.getElementById("b1");
 b1element.onclick = function() {
