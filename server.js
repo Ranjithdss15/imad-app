9d598262ebf4c61f8267a521589c5d89f60ba6f4
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-09-15 19:01:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.get('/testdb', function (req, res) {
 */
 
 
- pool.query('SELECT * FROM test NAME = ranjith', function(err, result) {
+ pool.query('SELECT * FROM test NAME order by Name', function(err, result) {
   if (err) {
     //console.log(err.stack);
     res.status(500).send(err.toString());
