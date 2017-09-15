57618f6f92afe20a150a4940532f5f3441aec2fb
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-09-15 21:13:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ var pool = new Pool(config);
 app.get('/testdb', function (req, res) {
 
 
- pool.query('SELECT * FROM test NAME order by Name', function(err, result) {
+ pool.query('SELECT * FROM test order by Name', function(err, result) {
   if (err) {
     //console.log(err.stack);
     res.status(500).send(err.toString());
