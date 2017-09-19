955e240030ceb5b4c3cf24084a2b10db8e7652da
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-09-19 18:43:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -94,7 +94,7 @@ submitelement.onclick = function() {
      console.log(username);
     console.log(password); 
     request.open('POST','http://ranjithdss15.imad.hasura-app.io/register',true);
-    request.setRequestHeader('Content-type','application/json');
+    request.setRequestHeader('Content-Type','application/json');
         request.send(JSON.stringify({username:username, password:password}));
         
 
