691f2ca2817505ec4e04e9061e71cf2ce4ad112d
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-09-19 18:47:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -151,6 +151,7 @@ app.get('/submit-comment/', function (req, res) {
     }
     
     app.post('/register', function (req, res) {
+        alert("into the module");
     console.log("regiteration module");
     username = req.body.username;
     password = req.body.password;
