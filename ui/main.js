bee2005685125b286558238b9bb184a601d34889
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-09-19 17:13:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -87,8 +87,8 @@ Submit.onclick = function() {
             } 
         }
     };
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+    var username = document.getElementById("uname").value;
+    var password = document.getElementById("pwrd").value;
     request.open('POST','http://ranjithdss15.imad.hasura-app.io/register',true);
         request.send(JSON.stringify({username:username,password:password}));
         
