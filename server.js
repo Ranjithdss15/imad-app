67d85885f09edfe5fe2aa854e3a0f4f9d31ed0d3
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-10-01 00:16:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -242,8 +242,6 @@ app.get('/ui/style.css', function (req, res) {
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-  app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/ui/URLshortner_bg.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'URLshortner_bg.jpg'));
