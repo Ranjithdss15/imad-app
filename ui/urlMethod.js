9fe5c28c9e2a200c65a8a0f0df7654e4f2d62902
Ranjithdss15 <30803859+Ranjithdss15@users.noreply.github.com>
2017-10-02 13:44:49 +0530

    [imad-console] Updates ui/urlMethod.js

--- ui/urlMethod.js
+++ ui/urlMethod.js
@@ -1,8 +1,71 @@
 console.log("URL method loaded");
+
+
+
 var test = document.getElementById('shorten');
 test.onclick = function() {
     alert("Loaded");
+    
+    
+     submitclick.onclick = function() {
+
+    var commentsip = document.getElementById('commentsinput');
+    commentsipvalue=commentsip.value;
+    if(commentsipvalue!=='')
+    {
+        var request =new XMLHttpRequest();
+        request.onreadystatechange =function() {
+            if(request.readyState === XMLHttpRequest.DONE) {
+                if(request.status ===200){
+                    var list = ' ';
+                var comments =  request.responseText;
+                comments=JSON.parse(comments);
+          for(var temp=0;temp<comments.length;++temp) {
+             list += ('<li>' + comments[temp]);
+              commentsop.innerHTML = list.toString();
+              commentsip.value="";
+            }
+            } 
+            }
+        };
+            request.open('GET','http://ranjithdss15.imad.hasura-app.io/submit-comment?comment='+commentsipvalue,true);
+            request.send(null);
+    }
 };
 
     
     
+    
+    
+    
+    
+};
+
+    
+    var commentsop = document.getElementById('commentsoutput');
+var submitclick = document.getElementById('commentsubmit');
+   submitclick.onclick = function() {
+
+    var commentsip = document.getElementById('commentsinput');
+    commentsipvalue=commentsip.value;
+    if(commentsipvalue!=='')
+    {
+        var request =new XMLHttpRequest();
+        request.onreadystatechange =function() {
+            if(request.readyState === XMLHttpRequest.DONE) {
+                if(request.status ===200){
+                    var list = ' ';
+                var comments =  request.responseText;
+                comments=JSON.parse(comments);
+          for(var temp=0;temp<comments.length;++temp) {
+             list += ('<li>' + comments[temp]);
+              commentsop.innerHTML = list.toString();
+              commentsip.value="";
+            }
+            } 
+            }
+        };
+            request.open('GET','http://ranjithdss15.imad.hasura-app.io/submit-comment?comment='+commentsipvalue,true);
+            request.send(null);
+    }
+};
